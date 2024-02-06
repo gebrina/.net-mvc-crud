d85a070c0487574aa4b00d7bb54bad30d1d1d168
gebrina <gsonofmika@gmail.com>
2024-02-06 21:24:52 +0300

    decorate employees required fields

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -10,17 +10,20 @@ namespace EmployeeMgmt.Models
         public Guid Id {get;set;}
         
         [Column(name:"first_name",TypeName ="varchar(25)")]
+        [Required(ErrorMessage ="First Name is required")]
         public string FristName {get;set;} = string.Empty;
 
+        [Required(ErrorMessage ="Last Name is required")]
         [Column(name:"last_name",TypeName ="varchar(25)")]
         public string LastName {get;set;} = string.Empty;
-
+ 
+        [Required(ErrorMessage ="Position is required")]
         [Column(name:"position",TypeName ="varchar(25)")]
         public string Position {get;set;} = string.Empty;
 
         [Column(name:"phone_number",TypeName ="varchar(25)")]
         public string PhoneNumber {get;set;} = string.Empty;
         public Guid CompanyId {get;set;}
-        public Company Company {get;set;}
+        public Company? Company {get;set;}
     }
 }
